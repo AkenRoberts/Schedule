cefe997e4cf27e5d07dbe87cde061b87f86d948f
Eric Aken Roberts <eric@cryode.com>
2014-09-19 17:43:13 -0500

    Add dd() helper (because Laravel habit)

--- index.php
+++ index.php
@@ -15,6 +15,20 @@ $run->pushHandler($handler);
 
 $run->register();
 
+if ( ! function_exists('dd'))
+{
+	/**
+	 * Dump the passed variables and end the script.
+	 *
+	 * @param  dynamic  mixed
+	 * @return void
+	 */
+	function dd()
+	{
+		array_map(function($x) { var_dump($x); }, func_get_args()); die;
+	}
+}
+
 // ------------------------------------------------------------------------
 
 $hours = new HoursArray(array(
