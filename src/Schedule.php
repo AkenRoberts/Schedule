83c9c2f0402a5b54202f913112e6ff542b340ea0
Eric Aken Roberts <eric@cryode.com>
2014-11-01 21:25:28 -0500

    Remove weekday constants.
    We'll stick with ISO-8601 standard for 1 (Mon) to 7 (Sun)

--- src/Schedule.php
+++ src/Schedule.php
@@ -4,14 +4,6 @@ use DateTime;
 
 class Schedule
 {
-    const SUNDAY    = 0;
-    const MONDAY    = 1;
-    const TUESDAY   = 2;
-    const WEDNESDAY = 3;
-    const THURSDAY  = 4;
-    const FRIDAY    = 5;
-    const SATURDAY  = 6;
-
     /**
      * Current time.
      *
